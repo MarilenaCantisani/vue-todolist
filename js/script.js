e52a2fe161fef2df54e8cc3eaa1cbb244b4deac2
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-29 14:48:50 +0200

    + Added file index.html / file style.css / file script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Configuration to activate "Vue" on the web inspector
+Vue.config.devtools = true;
+
+// Variable to activate "Vue"
+const root = new Vue({
+    el: "#root",
+    data: {
+
+    }
+});
