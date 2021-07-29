20ec5803628a5bd905718f2dcb980378679b726a
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-29 16:02:25 +0200

    + Added list with js and functions to add or delete items from the list

--- js/script.js
+++ js/script.js
@@ -5,6 +5,26 @@ Vue.config.devtools = true;
 const root = new Vue({
     el: "#root",
     data: {
-
+        //* To-do list
+        listItems: [
+            "Fare la spesa",
+            "Prenotare un appuntamento dal parrucchiere",
+            "Stendere la lavatrice",
+            "Controllare la posta",
+            "Comprare nuove tende in cucina",
+        ],
+        //* New things to do to list
+        newItem: "",
+    },
+    methods: {
+        //* Function that deletes things done from the list
+        removeItem(index) {
+            this.listItems.splice(index, 1);
+        },
+        //* Funzione that adds to the list new things to do 
+        addNewItem() {
+            this.listItems.push(this.newItem);
+            this.newItem = "";
+        }
     }
 });
